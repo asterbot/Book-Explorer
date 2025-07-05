60cf6b8ad8ce11deedebc9f4f2d67fc0d0f3ebc2
Skylar Ji <skylar.ji@uwaterloo.ca>
2025-07-05 16:59:43 -0400

    added author + publisher tables

--- data/drop_tables.sql
+++ data/drop_tables.sql
@@ -1,6 +1,7 @@
-DROP TABLE IF EXISTS `bookclub_members`;
-DROP TABLE IF EXISTS `bookclub_reads`;
-DROP TABLE IF EXISTS `bookclubs`;
+DROP TABLE IF EXISTS `book_publishers`;
+DROP TABLE IF EXISTS `publishers`;
+DROP TABLE IF EXISTS `book_authors`;
+DROP TABLE IF EXISTS `authors`;
 DROP TABLE IF EXISTS `userprogress`;
 DROP TABLE IF EXISTS `books`;
 DROP TABLE IF EXISTS `users`;
--- setup.sh
+++ setup.sh
@@ -9,7 +9,10 @@ mysql -u $1 -p cs348_project < data/drop_tables.sql
 mysql -u $1 -p cs348_project < data/books.sql
 mysql -u $1 -p cs348_project < data/users.sql
 mysql -u $1 -p cs348_project < data/userprogress.sql
-mysql -u $1 -p cs348_project < data/book_clubs.sql
+mysql -u $1 -p cs348_project < data/authors.sql
+mysql -u $1 -p cs348_project < data/book_authors.sql
+mysql -u $1 -p cs348_project < data/publishers.sql
+mysql -u $1 -p cs348_project < data/book_publishers.sql
 
 echo "Database setup complete"
 echo "Now set up .env as-per the README"
