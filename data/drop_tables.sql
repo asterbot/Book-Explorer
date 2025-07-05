8c81b6f1abb762ae3e1a6699bd729291589e3e79
garysu92 <garysu929292@gmail.com>
2025-07-04 22:09:01 -0400

    more entries and fix setup

--- data/drop_tables.sql
+++ data/drop_tables.sql
@@ -16,3 +16,9 @@ DROP TABLE IF EXISTS userprogress;
 
 DROP TABLE IF EXISTS books;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS `bookclub_members`;
+DROP TABLE IF EXISTS `bookclub_reads`;
+DROP TABLE IF EXISTS `bookclubs`;
+DROP TABLE IF EXISTS `userprogress`;
+DROP TABLE IF EXISTS `books`;
+DROP TABLE IF EXISTS `users`;
