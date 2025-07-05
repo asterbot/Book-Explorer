4fed00868a72ab0ddc3b5448ad84968d713b3ec4
Serena X <84420380+xuserena12@users.noreply.github.com>
2025-07-05 10:47:42 -0400

    added generation scripts

--- setup.sh
+++ setup.sh
@@ -9,7 +9,10 @@ mysql -u $1 -p cs348_project < data/drop_tables.sql
 mysql -u $1 -p cs348_project < data/books.sql
 mysql -u $1 -p cs348_project < data/users.sql
 mysql -u $1 -p cs348_project < data/userprogress.sql
-mysql -u $1 -p cs348_project < data/tags.sql
+mysql -u $1 -p cs348_project < data/tag.sql
+mysql -u $1 -p cs348_project < data/book_tag.sql
+mysql -u $1 -p cs348_project < data/user_book_tag.sql
+
 
 echo "Database setup complete"
 echo "Now set up .env as-per the README"
