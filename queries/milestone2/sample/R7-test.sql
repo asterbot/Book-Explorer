faa0eba4312a07baebbc45574d9c5792637421e0
Serena X <84420380+xuserena12@users.noreply.github.com>
2025-07-08 22:53:29 -0400

    Edit R7 sample test query

--- queries/milestone2/sample/R7-test.sql
+++ queries/milestone2/sample/R7-test.sql
@@ -1,3 +1,3 @@
 INSERT INTO userprogress (userID, bookID, status) 
 VALUES ((SELECT userID FROM users WHERE name = 'Alex'), '244', 'NOT STARTED')
-ON CONFLICT (userID, bookID) DO UPDATE SET status = EXCLUDED.status;
+ON CONFLICT (userID, bookID) DO UPDATE SET status = 'NOT STARTED';
