115bb970c5454764bd09019c4af82167965d7b3d
asterbot <arjunsodhi7@gmail.com>
2025-07-08 22:05:23 -0400

    modify R9

--- queries/milestone2/prod/R9-test.sql
+++ queries/milestone2/prod/R9-test.sql
@@ -3,4 +3,5 @@ SELECT b.bookID, b.title, AVG(ur.rating) as avg_rating
 FROM production.books b, production.UserRating ur
 WHERE b.bookID=ur.bookID
 GROUP BY b.bookID, b.title
+ORDER BY avg_rating DESC
 LIMIT 100;
